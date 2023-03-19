1e4ad7b7e434514d343457c225500638a1935419
kabachuha <artemkhrapov2001@yandex.ru>
2023-03-20 00:44:27 +0300

    add video stitching via ffmpeg

--- scripts/modelscope-text2vid.py
+++ scripts/modelscope-text2vid.py
@@ -9,6 +9,9 @@ from modules import lowvram, devices, sd_hijack
 from modules.shared import opts, cmd_opts, state, sd_model
 from scripts.t2v_pipeline import TextToVideoSynthesis, tensor2vid
 from webui import wrap_gradio_gpu_call
+import cv2
+
+outdir = os.path.join(opts.outdir_samples or opts.outdir_img2img_samples, 'text2video-modelscope')
 
 def setup_pipeline():
     pipe = TextToVideoSynthesis(ph.models_path+'/ModelScope/t2v')
@@ -16,7 +19,8 @@ def setup_pipeline():
 
 def process(prompt, n_prompt, steps, frames, cfg_scale, width=256, height=256, eta=0.0, cpu_vae=False):
     latents=None
-    # FIXME
+
+    # FIXME sd unload
     #lowvram.send_everything_to_cpu()
     #sd_hijack.model_hijack.undo_hijack(sd_model)
     #devices.torch_gc()
@@ -25,13 +29,17 @@ def process(prompt, n_prompt, steps, frames, cfg_scale, width=256, height=256, e
     print('Pipeline setup')
     pipe = setup_pipeline()
     print('Starting text2video')
-    print(pipe.infer(prompt, n_prompt, steps, frames, cfg_scale, width, height, eta, cpu_vae, latents))
-    raise 'd'
-    video_path, _ = pipe.infer(prompt, n_prompt, steps, frames, cfg_scale, width, height, eta, cpu_vae, latents)
-    print(f't2v complete, result saved at {video_path}')
-    print(video_path)
+    #print(pipe.infer(prompt, n_prompt, steps, frames, cfg_scale, width, height, eta, cpu_vae, latents))
+    samples, _ = pipe.infer(prompt, n_prompt, steps, frames, cfg_scale, width, height, eta, cpu_vae, latents)
+    for i in range(len(samples)):
+        cv2.imwrite(outdir + os.path.sep + f"{i:09}.png", samples[i])
+    
+    # TODO: add params to the GUI
 
-    #devices.torch_gc()
+    ffmpeg_stitch_video('ffmpeg', 24, outdir + os.path.sep + f"vid.mp4", 0, None, outdir)# add timestring
+    print(f't2v complete, result saved at {outdir}')
+
+    devices.torch_gc()
     #lowvram.setup_for_low_vram(sd_model, cmd_opts.medvram)
     #sd_hijack.model_hijack.hijack(sd_model)
 
@@ -106,3 +114,78 @@ def on_ui_tabs():
     return [(deforum_interface, "ModelScope text2video", "t2v_interface")]
 
 script_callbacks.on_ui_tabs(on_ui_tabs)
+
+import os, subprocess, time, cons
+
+# Stitch images to a h264 mp4 video using ffmpeg
+def ffmpeg_stitch_video(ffmpeg_location=None, fps=None, outmp4_path=None, stitch_from_frame=0, stitch_to_frame=None, imgs_path=None, add_soundtrack=None, audio_path=None, crf=17, preset='veryslow'):
+    start_time = time.time()
+
+    print(f"Got a request to stitch frames to video using FFmpeg.\nFrames:\n{imgs_path}\nTo Video:\n{outmp4_path}")
+    msg_to_print = f"Stitching *video*..."
+    print(msg_to_print)
+    if stitch_to_frame == -1:
+        stitch_to_frame = 999999999
+    try:
+        cmd = [
+            ffmpeg_location,
+            '-y',
+            '-vcodec', 'png',
+            '-r', str(float(fps)),
+            '-start_number', str(stitch_from_frame),
+            '-i', imgs_path,
+            '-frames:v', str(stitch_to_frame),
+            '-c:v', 'libx264',
+            '-vf',
+            f'fps={float(fps)}',
+            '-pix_fmt', 'yuv420p',
+            '-crf', str(crf),
+            '-preset', preset,
+            '-pattern_type', 'sequence',
+            outmp4_path
+        ]
+        process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = process.communicate()
+    except FileNotFoundError:
+        print("\r" + " " * len(msg_to_print), end="", flush=True)
+        print(f"\r{msg_to_print}", flush=True)
+        raise FileNotFoundError("FFmpeg not found. Please make sure you have a working ffmpeg path under 'ffmpeg_location' parameter.")
+    except Exception as e:
+        print("\r" + " " * len(msg_to_print), end="", flush=True)
+        print(f"\r{msg_to_print}", flush=True)
+        raise Exception(f'Error stitching frames to video. Actual runtime error:{e}')
+    
+    if add_soundtrack != 'None':
+        audio_add_start_time = time.time()
+        try:
+            cmd = [
+                ffmpeg_location,
+                '-i',
+                outmp4_path,
+                '-i',
+                audio_path,
+                '-map', '0:v',
+                '-map', '1:a',
+                '-c:v', 'copy',
+                '-shortest',
+                outmp4_path+'.temp.mp4'
+            ]
+            process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            stdout, stderr = process.communicate()
+            if process.returncode != 0:
+                print("\r" + " " * len(msg_to_print), end="", flush=True)
+                print(f"\r{msg_to_print}", flush=True)
+                raise RuntimeError(stderr)
+            os.replace(outmp4_path+'.temp.mp4', outmp4_path)
+            print("\r" + " " * len(msg_to_print), end="", flush=True)
+            print(f"\r{msg_to_print}", flush=True)
+            print(f"\rFFmpeg Video+Audio stitching \033[0;32mdone\033[0m in {time.time() - start_time:.2f} seconds!", flush=True)
+        except Exception as e:
+            print("\r" + " " * len(msg_to_print), end="", flush=True)
+            print(f"\r{msg_to_print}", flush=True)
+            print(f'\rError adding audio to video. Actual error: {e}', flush=True)
+            print(f"FFMPEG Video (sorry, no audio) stitching \033[33mdone\033[0m in {time.time() - start_time:.2f} seconds!", flush=True)
+    else:
+        print("\r" + " " * len(msg_to_print), end="", flush=True)
+        print(f"\r{msg_to_print}", flush=True)
+        print(f"\rVideo stitching \033[0;32mdone\033[0m in {time.time() - start_time:.2f} seconds!", flush=True)
