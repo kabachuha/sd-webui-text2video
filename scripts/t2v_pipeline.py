fdf507c7e0672455020e36f4c9d839a0a22abb1e
kabachuha <artemkhrapov2001@yandex.ru>
2023-03-26 00:39:34 +0300

    deduplicate torch_gc

--- scripts/t2v_pipeline.py
+++ scripts/t2v_pipeline.py
@@ -18,30 +18,7 @@ from modules import devices
 
 __all__ = ['TextToVideoSynthesis']
 
-try:
-    import gc
-    import torch
-
-    def torch_gc():
-        """Performs garbage collection for both Python and PyTorch CUDA tensors.
-
-        This function collects Python garbage and clears the PyTorch CUDA cache
-        and IPC (Inter-Process Communication) resources.
-        """
-        gc.collect()  # Collect Python garbage
-        torch.cuda.empty_cache()  # Clear PyTorch CUDA cache
-        torch.cuda.ipc_collect()  # Clear PyTorch CUDA IPC resources
-
-except:
-
-    def torch_gc():
-        """Dummy function when torch is not available.
-
-        This function does nothing and serves as a placeholder when torch is
-        not available, allowing the rest of the code to run without errors.
-        """
-        pass
-
+from scripts.t2v_model import torch_gc
 
 class TextToVideoSynthesis():
     r"""
