8f8a6690e41b2e9eeae08c6b8c251a2ea4a2a248
kabachuha <artemkhrapov2001@yandex.ru>
2023-03-26 13:36:49 +0300

    Update t2v_model.py

--- scripts/t2v_model.py
+++ scripts/t2v_model.py
@@ -1371,8 +1371,8 @@ class GaussianDiffusion(object):
 
         pbar = tqdm(steps, desc="DDIM sampling")
 
-        print(c)
-        print(uc)
+        #print(c)
+        #print(uc)
 
         i = 0
         for step in pbar:
@@ -1389,7 +1389,7 @@ class GaussianDiffusion(object):
             elif uc_i.shape[1] > c_i.shape[1]:
                 uc_i = uc_i[:, :c_i.shape[1]]
             
-            #print(c_i.shape, uc.shape)
+            print(c_i.shape, uc.shape)
 
             t = torch.full((b, ), step, dtype=torch.long, device=xt.device)
             print(uc_i)
