130b9af31d2f217247ae7e9cb46f0b3f972e0eb3
kabachuha <artemkhrapov2001@yandex.ru>
2023-08-31 00:06:51 +0300

    fixup defaulting

--- scripts/modelscope/process_modelscope.py
+++ scripts/modelscope/process_modelscope.py
@@ -241,7 +241,12 @@ def process_modelscope(args_dict, extra_args=None):
 
         # TODO: add params to the GUI
         if not video_args.skip_video_creation:
-            metadata = { 'comment': infotext } if opts.data is not None and opts.data.get("modelscope_save_metadata") else None
+            metadata = None
+            if opts.data is not None and opts.data.get("modelscope_save_metadata") is not None:
+                if opts.data.get("modelscope_save_metadata"):
+                    metadata = { 'parameters': infotext }
+            else:
+                metadata = { 'parameters': infotext }
             ffmpeg_stitch_video(ffmpeg_location=video_args.ffmpeg_location, fps=video_args.fps, outmp4_path=outdir_current + os.path.sep + f"vid.mp4", imgs_path=os.path.join(outdir_current,
                                                                                                                                                                               "%06d.png"),
                                 stitch_from_frame=0, stitch_to_frame=-1, add_soundtrack=video_args.add_soundtrack,
